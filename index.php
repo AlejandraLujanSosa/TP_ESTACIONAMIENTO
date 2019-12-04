587cbd8c23a96c16f0b9d29d944a1927dae053d0
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2019-12-04 19:54:43 -0300

    cambio de ruta

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
     <title>ESTACIONAMIENTO WERPI</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="/ccs/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="sticky-footer-navbar.css" rel="stylesheet">
+    <link href="/ccs/sticky-footer-navbar.css" rel="stylesheet">
   </head>
 
   <body background="https://media.istockphoto.com/photos/grey-gradient-abstract-background-for-background-or-wallpaper-your-picture-id853598200?k=6&m=853598200&s=170667a&w=0&h=zefhWQKtIKaiz2O5lr1NR704N8XVDugKutllNfyvm2g=">
@@ -34,7 +34,7 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="popper.min.js"></script>
-    <script src="popper.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/popper.min.js"></script>
   </body>
 </html>
