a3bbcb9348b8836939475f182b325953b20e3b2e
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2019-12-11 19:44:38 -0300

    cambio

--- funciones/HacerRegistro.php
+++ funciones/HacerRegistro.php
@@ -1,14 +1,17 @@
 <?php
-
+echo "hola";
+die();
 session_start();
-include '../AccesoDatos.php';
+ include('../headers/Header.php'); 
+include '../DB/AccesoDatos.php';
 
 $miobjeto = new stdClass();
 $miobjeto->Usuario=$_GET['Usuario'];
 $miobjeto->Clave=$_GET['Clave'];
+var_dump($miobjeto);
 
 $query =$BaseDeDatos->prepare("INSERT INTO Usuarios (nombre,clave) VALUES ('$miobjeto->Usuario','$miobjeto->Clave')");
 $query->execute();	
 
-header("Location:..\paginas\RegistroOK.php");
+header("Location:../paginas/RegistroOK.php");
 ?>
