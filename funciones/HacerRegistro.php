197987da0509356e46ed34f41e1afdc00eeee907
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2019-12-11 20:42:31 -0300

    Add files via upload

--- funciones/HacerRegistro.php
+++ funciones/HacerRegistro.php
@@ -3,12 +3,12 @@
 //
 //session_start();
  include('../headers/Header.php'); 
- include '../DB/AccesoDatos.php';
+ include 'AccesoDatos.php';
 
 $miobjeto = new stdClass();
 $miobjeto->Usuario=$_GET['Usuario'];
 $miobjeto->Clave=$_GET['Clave'];
-var_dump($miobjeto);
+//var_dump($miobjeto);
 die();
 $query =$BaseDeDatos->prepare("INSERT INTO Usuarios (nombre,clave) VALUES ('$miobjeto->Usuario','$miobjeto->Clave')");
 $query->execute();	
